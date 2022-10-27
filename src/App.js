0b5f8bb3d4516087930bf3c1aff63adb734dfb75
Tom Heafey <tmheafey@gmail.com>
2022-10-27 12:07:19 -0500

    adds basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { Navigate, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/LoginPage";
+import SearchPage from "./components/SearchPage";
+import DetailPage from "./components/DetailPage";
 
 function App() {
-    return <LoginPage />;
+    return (
+        <Router>
+            {/* navbar */}
+            <Routes>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+                <Route path="/search">
+                    <SearchPage />
+                </Route>
+                <Route path="/detail">
+                    <DetailPage />
+                </Route>
+                <Route path="*">
+                    <Navigate to="/search" />
+                </Route>
+            </Routes>
+        </Router>
+    );
+
+    // return <LoginPage />;
 }
 
 export default App;
---
+++ src/shared/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import DetailPage from "../../components/DetailPage";
+import LoginPage from "../../components/LoginPage";
+import SearchPage from "../../components/SearchPage";
+
+import React from "react";
+
+const ProtectedRoute = () => {
+    return <div>ProtectedRoute</div>;
+};
+
+// export default ProtectedRoute
