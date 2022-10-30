d4db661911208f540b8e6cf6b1dbc728347a0451
Tom Heafey <tmheafey@gmail.com>
2022-10-30 14:27:06 -0500

    adds styling to login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { setAuth, revokeAuth } from "../shared/redux/authSlice";
+import "../shared/css/LoginPage.css";
 
 const LoginPage = ({ setAuth }) => {
     const [password, setPassword] = useState("");
     //set auth state when password === env.pw?
 
     return (
-        <>
+        <div className="input-container">
             <div>speak friend and enter</div>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button
-                id="login"
-                onClick={() => {
-                    if (password === process.env.REACT_APP_PASSWORD) {
-                        console.log("authorized");
-                        return setAuth();
-                    }
-                    return;
-                }}
-            >
-                Enter
-            </button>
-        </>
+            <div>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+
+            <div>
+                <button
+                    id="login"
+                    onClick={() => {
+                        if (password === process.env.REACT_APP_PASSWORD) {
+                            console.log("authorized");
+                            return setAuth();
+                        }
+                        return;
+                    }}
+                >
+                    Enter
+                </button>
+            </div>
+        </div>
     );
 };
 
